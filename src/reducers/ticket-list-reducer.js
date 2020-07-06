8a191ae62f195584dc1fe580b4dc9b1f374032ba
austinb847 <austinb847@icloud.com>
2020-07-06 13:09:05 -0700

    handle adding a new ticket

--- src/reducers/ticket-list-reducer.js
+++ src/reducers/ticket-list-reducer.js
@@ -1,3 +1,17 @@
 export default (state = {}, action) => {
-  return state;
+  const { names, location, issue, id } = action;
+  switch(action.type) {
+    case 'ADD_TICKET':
+      return {
+        ...state,
+        [id]: {
+          names: names,
+          location: location,
+          issue: issue,
+          id: id
+        }
+      };
+    default:
+      return state;
+  }
 };
